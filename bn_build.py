329ff4eb35157917603d0740c123231b5da4a146
Nick Zhang <zhangxy.pro@gmail.com>
2015-12-27 13:41:17 +0800

    add p->e edges

--- bn_build.py
+++ bn_build.py
@@ -9,6 +9,7 @@ class Vertex_E(object):
         self.idx = idx
         self.prop = prop
         self.parent = {}
+        self.e_parent_cnt = 0
         self.child = {}
         self.layer = -1 # 拓扑排序中的层数
 
@@ -28,31 +29,28 @@ class Vertex_E(object):
          # if multi edges exist, choose the one with largest weight
         if self.parent.has_key(p_id) and self.parent[p_id][1] >= w:
             pass
-            # continue
-            # print "double edge!!",p.idx,self.idx,w
-            # self.parent[p_id].append([p,w])
         else:
             self.parent[p_id] = [p,w]
+            if p_id[-1] == 'E':
+                self.e_parent_cnt += 1
 
     def set_child(self,c,w):
         c_id = c.idx
         # if multi edges exist, choose the one with largest weight
         if self.child.has_key(c_id) and self.child[c_id][1] >= w:
             pass
-            # continue
-            # print "double edge!!",self.idx,c.idx,w
-            # self.child[c_id].append([c,w])
         else:
             self.child[c_id] = [c,w]
 
     def print_vertex(self):
-        print "vertex name:",self.idx
+        print "=====","vertex name:",self.idx,"====="
         print "layer in topSort:",self.layer
         print "Parents:", ",".join([v + " " + str(self.parent[v][1]) for v in self.parent])
+        print "No. of E parents:",self.e_parent_cnt
         print "Child:", ",".join([v + " " + str(self.child[v][1]) for v in self.child])
 
     def set_root_marginal_dist(self):
-        prob = sigmoid(self.norm_score)
+        prob = sigmoid(self.norm_score) # 更改此函数替换分数与概率的对应
         self.root_marginal_dist = pd.DataFrame({'P':[prob,1-prob],self.idx:[0,1]},columns = ['P',self.idx])
 
     def set_global_lpd(self):
@@ -60,17 +58,29 @@ class Vertex_E(object):
         parent_id_list = []
         parent_weight_list = []
         parent_df_list = []
+        p_parent_weight = 0
 
         for k in self.parent:
+            if k[-1] == 'P':
+                p_parent_weight += self.parent[k][1]
+                continue
             if not self.parent[k][0]._no_score_in_root:
                 parent_id_list.append(k)
                 parent_weight_list.append(self.parent[k][1])
                 parent_df_list.append(self.parent[k][0].global_marginal_dist)
 
+        if p_parent_weight > 0 and self.creditscore > -1:
+            # 在e父亲节点后加入p父亲节点的加总，名称为P_parents,权重为p节点的加总，其df_list为当前节点的原始边缘概率
+            parent_id_list.append('P_parents')
+            parent_weight_list.append(p_parent_weight)
+            p_pa_marginal_df = self.root_marginal_dist.copy()
+            p_pa_marginal_df.columns = ['P','P_parents']
+            parent_df_list.append(p_pa_marginal_df)
+
         num_of_val_parents = len(parent_id_list)
-        print "num of parents:",len(self.parent),len(parent_id_list)
+        print "num of parents:",self.e_parent_cnt,num_of_val_parents
         if num_of_val_parents > 11:
-            print "too many parents! parent number:",len(parent_id_list)
+            print "too many parents! parent number:",num_of_val_parents
             return self.root_marginal_dist,[]
 
         # 将边权重归一化
@@ -79,7 +89,6 @@ class Vertex_E(object):
         # print parent_id_list,parent_weight_list,parent_df_list
 
         # 求得lambda
-        # print "cal lambda:"
         para_list = np.array(cal_parameter_list(parent_id_list,parent_weight_list,parent_df_list))
         # print para_list
 
@@ -88,19 +97,25 @@ class Vertex_E(object):
         lpd_df = generate_df(lpd_cols)
 
         # 目前认为若父节点均不出问题，则不会出问题，即norisy-or中lambda0 = 0
-        lpd_df.ix[0]['P'] = 1
+        # 此处 leak_prob = P(y=0|pa(y) = 0)
+        if self.creditscore > -1 and num_of_val_parents > 1:
+            # leak_prob = self.root_marginal_dist[self.root_marginal_dist[self.idx] == 0]['P']
+            leak_prob = 0.9
+        else:
+            # 对无原始分及父亲只有一个的节点，lambda0 = 1-leak_prob = 0；父亲只有一个的节点，其para为1，若存在leak则客观上降低其变好的概率
+            leak_prob = 1
+        # leak_prob = 1
+        lpd_df.ix[0]['P'] = leak_prob
         for i in range(1,lpd_df.shape[0]):
             if lpd_df.ix[i][2:].sum() == 0 and lpd_df.ix[i][self.idx] == 1:
-                lpd_df.ix[i]['P'] = 0
+                lpd_df.ix[i]['P'] = 1 - leak_prob
                 continue
-
             # 对于父节点数量为1的，直接设定；用reduce计算
             if lpd_df.ix[i][self.idx] == 0:
-                lpd_df.ix[i]['P'] = 0 if num_of_val_parents == 1 else reduce(lambda a,b:a*b,[f for f in lpd_df.ix[i][2:] * (1 - para_list) if f > 0])
+                lpd_df.ix[i]['P'] = (1 - para_list[0]) if num_of_val_parents == 1 else reduce(lambda a,b:a*b,[f for f in lpd_df.ix[i][2:] * (1 - para_list) if f > 0])
             else:
-                lpd_df.ix[i]['P'] = 1 if num_of_val_parents == 1 else 1 - reduce(lambda a,b:a*b,[f for f in lpd_df.ix[i][2:] * (1 - para_list) if f > 0])
+                lpd_df.ix[i]['P'] = 1 - (1 - para_list[0]) if num_of_val_parents == 1 else 1 - reduce(lambda a,b:a*b,[f for f in lpd_df.ix[i][2:] * (1 - para_list) if f > 0])
 
-        # print lpd_df
         return lpd_df,parent_df_list
 
     def set_global_marginal_dist(self,is_root):
@@ -115,15 +130,10 @@ class Vertex_E(object):
                 joint_df = self.global_lpd.copy()
                 for p_df in parent_df_list:
                     joint_df = multiplyDf(joint_df,p_df)
-                # print "joint_df complete"
-                # print joint_df
                 marginal_df = joint_df.groupby(self.idx).sum()['P'].reset_index() # 层次化索引转为列
                 marginal_df = marginal_df.reindex_axis(['P',self.idx],1) # 调整列顺序
-                # print "marginal_df"
-                # print marginal_df
-                # print "ori:"
-                # print self.root_marginal_dist
                 self.global_marginal_dist = marginal_df
+
             else:
                 print "no lpd computed because of too many parents"
                 self.global_marginal_dist = lpd
@@ -147,17 +157,24 @@ class Graph(object):
         self._min_score = 0 # 记录原始评分的最大最小分值，依次转换求出的边缘概率，得到新的分数
         self._max_score = 0
 
-    def set_edge(self,parent,child,w):
-        p_v = self.vertex_list[parent]
+    def set_edge(self,parent,child,w,link_void_e_cnt):
         c_v = self.vertex_list[child]
-        p_v.set_child(c_v,w)
-        c_v.set_parent(p_v,w)
+        if parent[-1] == 'P':
+            p_v = Vertex_E(parent,'P',-2,None) # 生成虚的P节点，根据prop确定是否为E
+            c_v.set_parent(p_v,w)
+        elif (parent[-1] == 'E') and (self.vertex_list[parent].creditscore > -1): # 忽略信用分缺失的父节点边，共661条
+            p_v = self.vertex_list[parent]
+            p_v.set_child(c_v,w)
+            c_v.set_parent(p_v,w)
+        else:
+            link_void_e_cnt += 1
+        return link_void_e_cnt
 
     def get_root(self):
         self.root_list = []
         for v_id in self.vertex_list:
             v = self.vertex_list[v_id]
-            if v.prop == 'E' and len(v.parent) == 0:
+            if v.prop == 'E' and v.e_parent_cnt == 0:
                 v.layer = 0
                 self.root_list.append(v)
 
@@ -177,7 +194,7 @@ class Graph(object):
                 if len(v.child) > 0:
                     for chv_id in v.child:
                         chv = v.child[chv_id][0]
-                        ch_parent_set = set([chv.parent[chp][0] for chp in chv.parent])
+                        ch_parent_set = set([chv.parent[chp][0] for chp in chv.parent if chp[-1] == 'E'])
                         if ((ch_parent_set & set(visited_list)) == ch_parent_set) and (chv not in vertex_layers[layer]):
                             vertex_layers[layer].append(chv)
                             chv.layer = layer
@@ -185,12 +202,12 @@ class Graph(object):
         return vertex_layers
 
     def normalize_score(self):
-        score_list = [self.vertex_list[v].creditscore for v in self.vertex_list if self.vertex_list[v].creditscore != -1]
+        score_list = [self.vertex_list[v].creditscore for v in self.vertex_list if self.vertex_list[v].creditscore > -1]
         min_score = min(score_list)
         max_score = max(score_list)
         max_min = max_score - min_score
         for v_id in self.vertex_list:
-            if self.vertex_list[v_id].creditscore != -1:
+            if self.vertex_list[v_id].creditscore > -1:
                 self.vertex_list[v_id].norm_score = (self.vertex_list[v_id].creditscore - min_score) / max_min
                 self.vertex_list[v_id].set_root_marginal_dist()
             else:
@@ -200,9 +217,9 @@ class Graph(object):
 
     def build_global_bn(self,layer_subgraph):
         print "==== start to build global bayesian model ===="
-        subgroph_cnt = len(layer_subgraph)- 1
-        print subgroph_cnt
-        for i in range(subgroph_cnt):
+        subgraph_cnt = len(layer_subgraph) - 1
+        print subgraph_cnt
+        for i in range(subgraph_cnt):
             cur_vertex_list = layer_subgraph[i]
             for cur_v in cur_vertex_list:
                 if i == 0:
@@ -213,7 +230,6 @@ class Graph(object):
                     continue
                 print "layer:",i,cur_v.idx
                 cur_v.set_global_marginal_dist(0)
-                # break
         print "==== global bayesian model built successfully! ===="
 
     def convert_to_new_score(self):
@@ -233,7 +249,7 @@ class Graph(object):
                 new_score = norm_new_score * (self._max_score - self._min_score) + self._min_score
                 v.global_new_score = round(new_score,2)
             # print v.idx,v.creditscore,v.global_new_score
-        print "==new scores generated=="
+        print "== new scores generated =="
 
     def print_bn_result(self):
         print "node,layer,original_score,new_score,diff(new-origin)"
@@ -253,7 +269,7 @@ class Graph(object):
                 self.vertex_list[v].print_vertex()
 
 def sigmoid(x):
-    return math.e ** x / (1 + math.e ** x)
+    return 1 / (1 + math.e ** (-1 * x))
 
 def is_P(v):
     return v[-1] == 'P'
@@ -302,35 +318,37 @@ def load_vertex_P(vertex_fn, prop, v_list):
 def load_link(link_fn, graph):
     link_file = open(link_fn, 'r')
     link_file.readline()
-    link_pe_cnt = 0
     link_cnt = 0
-    link_other_cnt = 0
+    link_other_cnt = 0 # 无向边计数
+    link_ep_cnt = 0 # e->p边计数
+    link_void_e_cnt = 0 # e->e边父节点无原始分边计数
     for line in link_file.readlines():
         link_cnt += 1
         link = line.strip().split(',')
-        if link[1].strip('"') == 'P' or link[3].strip('"') == 'P':
-            link_pe_cnt += 1
-            # print line
-            continue
-        v1 = link[0].strip('"')+link[1].strip('"')
-        v2 = link[2].strip('"')+link[3].strip('"')
+        v1 = link[0].strip('"') + link[1].strip('"')
+        v2 = link[2].strip('"') + link[3].strip('"')
         link_weight = float(link[4].strip('"'))
         link_prop_pos = link[5].strip('"')
 
-        # 暂时忽略以空节点为父的边
+        # 跳过无向边，无向边共3049条
+        if link_prop_pos == 'OTHER':
+            link_other_cnt += 1
+            continue
+
+        if v1[-1] == 'P' and v2[-1] == 'P':
+            print line
+            continue
+
+        # 只保留儿子为E的边
         if link_weight != 0:
-            if link_prop_pos == 'FATHER' and graph.vertex_list[v1].creditscore > -1:
-                graph.set_edge(v1,v2,link_weight)
-            elif link_prop_pos == 'SON' and graph.vertex_list[v2].creditscore > -1:
-                graph.set_edge(v2,v1,link_weight)
-            elif link_prop_pos == 'OTHER': # no OTHER E-E edge!!
-                link_other_cnt += 1
-                graph.set_edge(v1,v2,link_weight)
-                graph.set_edge(v2,v1,link_weight)
+            if link_prop_pos == 'FATHER' and v2[-1] == 'E':
+                link_void_e_cnt = graph.set_edge(v1,v2,link_weight,link_void_e_cnt)
+            elif link_prop_pos == 'SON' and v1[-1] == 'E':
+                link_void_e_cnt = graph.set_edge(v2,v1,link_weight,link_void_e_cnt)
             else:
-                pass
-                # print 'wrong link property!!!',line
-    print link_cnt,link_pe_cnt,link_other_cnt # 45831 37918 0
+                link_ep_cnt += 1 # 观察发现无E->P的有向边
+                print 'son is p',line
+    print link_cnt,link_other_cnt,link_ep_cnt,link_void_e_cnt # 45831 3049 0 659
     return graph
 
 def cal_parameter_list(id_list,weight_list,df_list):
@@ -373,13 +391,13 @@ if __name__ == '__main__':
     graph = load_link("./data/LINK_ANON.csv", graph)
     # for v in graph.vertex_list:
     #     graph.vertex_list[v].print_vertex()
-    # graph.normalize_score()
-    # graph.get_root()
-    # print "root cnt:",len(graph.root_list)
+    graph.normalize_score()
+    graph.get_root()
+    print "root cnt:",len(graph.root_list)
 
-    # layer_subgraph = graph.top_sort()
-    # # query_node_list = ["anon_S4847E","anon_S2162E","anon_S5175E","anon_S6E","anon_S2047E","anon_S1457E","anon_S4118E","anon_S51E","anon_S5559E","anon_S2086E","anon_S3263E","anon_S2766E","anon_S5014E"]
-    # # graph.get_local_structure(query_node_list)
-    # graph.build_global_bn(layer_subgraph)
-    # graph.convert_to_new_score()
-    # graph.print_bn_result()
+    layer_subgraph = graph.top_sort()
+    # query_node_list = ["anon_S516E"]
+    # graph.get_local_structure(query_node_list)
+    graph.build_global_bn(layer_subgraph)
+    graph.convert_to_new_score()
+    graph.print_bn_result()
